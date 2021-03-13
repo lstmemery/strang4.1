a54c6c094791ca3c6ebe3565ef96fed4f9df0f20
lstmemery <matthew.emery44@gmail.com>
2021-03-13 09:42:26 -0800

    Initial commit

---
+++ strang4.1.jl
@@ -0,0 +1,11 @@
+### A Pluto.jl notebook ###
+# v0.12.21
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 3bceafac-8423-11eb-004f-2b5764c25b2e
+
+
+# ╔═╡ Cell order:
+# ╠═3bceafac-8423-11eb-004f-2b5764c25b2e
